9a10a0151fd3f701f60728190b7c67cb005663d1
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:46 +0545

    improve

--- ext-libs/twisted/enterprise/__init__.py
+++ ext-libs/twisted/enterprise/__init__.py
@@ -4,6 +4,10 @@
 
 """
 Twisted Enterprise: database support for Twisted services.
+This package provides an adapter layer between Twisted and various SQL databases,
+using the popular DB-API 2.0 interface.
 """
 
 __all__ = ['adbapi']
+__version__ = '1.0.0'  # replace with the actual version
+
