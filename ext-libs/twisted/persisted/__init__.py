934442777f4df7130c93d69c72ef4874ed64cc6b
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:13 +0545

    improve

--- ext-libs/twisted/persisted/__init__.py
+++ ext-libs/twisted/persisted/__init__.py
@@ -4,3 +4,47 @@
 """
 Twisted Persisted: utilities for managing persistence.
 """
+
+import os
+from zope.interface import implementer
+from zope.schema import TextLine, Text
+from persistent import Persistent
+from BTrees.OOBTree import OOBTree
+
+from twisted.spread import avatar
+from twisted.persisted.styles import IStyle
+from twisted.python.components import registerAdapter
+
+
+@implementer(IStyle)
+class JsonStyle(object):
+    """JSON style for persisting objects."""
+
+    def __init__(self, path):
+        """Create a new JSON style with the given path.
+
+        Args:
+            path (str): The file path to save the persisted data.
+        """
+        self.path = path
+
+    def encode(self, object):
+        """Encode the given object to a JSON string.
+
+        Args:
+            object: The object to encode.
+
+        Returns:
+            str: The JSON string representation of the object.
+        """
+        return json.dumps(object)
+
+    def decode(self, string):
+        """Decode the given JSON string to an object.
+
+        Args:
+            string (str): The JSON string to decode.
+
+        Returns:
+            object: The Python object representation of the JSON string.
+       
