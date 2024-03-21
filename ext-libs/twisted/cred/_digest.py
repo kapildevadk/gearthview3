3e684604eee0a87d2c7292f689b3df4ef37141b4
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:00 +0545

    improve

--- ext-libs/twisted/cred/_digest.py
+++ ext-libs/twisted/cred/_digest.py
@@ -8,122 +8,139 @@ Calculations for HTTP Digest authentication.
 @see: U{http://www.faqs.org/rfcs/rfc2617.html}
 """
 
-from twisted.python.hashlib import md5, sha1
+from typing import Any, BinaryIO, Optional
 
+import hashlib
 
-
-# The digest math
 
 algorithms = {
-    'md5': md5,
-
-    # md5-sess is more complicated than just another algorithm.  It requires
-    # H(A1) state to be remembered from the first WWW-Authenticate challenge
-    # issued and re-used to process any Authorization header in response to
-    # that WWW-Authenticate challenge.  It is *not* correct to simply
-    # recalculate H(A1) each time an Authorization header is received.  Read
-    # RFC 2617, section 3.2.2.2 and do not try to make DigestCredentialFactory
-    # support this unless you completely understand it. -exarkun
-    'md5-sess': md5,
-
-    'sha': sha1,
+    "md5": hashlib.md5,
+    "md5-sess": hashlib.md5,
+    "sha": hashlib.sha1,
 }
 
-# DigestCalcHA1
-def calcHA1(pszAlg, pszUserName, pszRealm, pszPassword, pszNonce, pszCNonce,
-            preHA1=None):
+
+def calcHA1(
+    algo: str,
+    user_name: Optional[str],
+    realm: Optional[str],
+    password: Optional[str],
+    nonce: Optional[str],
+    cnonce: Optional[str],
+    preHA1: Optional[str] = None,
+) -> str:
     """
     Compute H(A1) from RFC 2617.
 
-    @param pszAlg: The name of the algorithm to use to calculate the digest.
+    @param algo: The name of the algorithm to use to calculate the digest.
         Currently supported are md5, md5-sess, and sha.
-    @param pszUserName: The username
-    @param pszRealm: The realm
-    @param pszPassword: The password
-    @param pszNonce: The nonce
-    @param pszCNonce: The cnonce
-
+    @param user_name: The username
+    @param realm: The realm
+    @param password: The password
+    @param nonce: The nonce
+    @param cnonce: The cnonce
     @param preHA1: If available this is a str containing a previously
-       calculated H(A1) as a hex string.  If this is given then the values for
-       pszUserName, pszRealm, and pszPassword must be C{None} and are ignored.
+        calculated H(A1) as a hex string. If this is given then the values for
+        user_name, realm, and password must be C{None} and are ignored.
     """
-
-    if (preHA1 and (pszUserName or pszRealm or pszPassword)):
-        raise TypeError(("preHA1 is incompatible with the pszUserName, "
-                         "pszRealm, and pszPassword arguments"))
+    if preHA1 and (user_name or realm or password):
+        raise ValueError("preHA1 is incompatible with the user_name, " "realm, and password arguments")
 
     if preHA1 is None:
-        # We need to calculate the HA1 from the username:realm:password
-        m = algorithms[pszAlg]()
-        m.update(pszUserName)
-        m.update(":")
-        m.update(pszRealm)
-        m.update(":")
-        m.update(pszPassword)
+        m = algorithms[algo]()
+        m.update(user_name.encode())
+        m.update(b":")
+        m.update(realm.encode())
+        m.update(b":")
+        m.update(password.encode())
         HA1 = m.digest()
     else:
-        # We were given a username:realm:password
-        HA1 = preHA1.decode('hex')
+        HA1 = bytes.fromhex(preHA1)
 
-    if pszAlg == "md5-sess":
-        m = algorithms[pszAlg]()
+    if algo == "md5-sess":
+        m = algorithms[algo]()
         m.update(HA1)
-        m.update(":")
-        m.update(pszNonce)
-        m.update(":")
-        m.update(pszCNonce)
+        m.update(nonce.encode())
+        m.update(b":")
+        m.update(cnonce.encode())
         HA1 = m.digest()
 
-    return HA1.encode('hex')
+    return HA1.hex()
 
 
-def calcHA2(algo, pszMethod, pszDigestUri, pszQop, pszHEntity):
+def calcHA2(
+    algo: str, method: str, digest_uri: str, qop: str, entity: Optional[BinaryIO] = None
+) -> str:
     """
     Compute H(A2) from RFC 2617.
 
-    @param pszAlg: The name of the algorithm to use to calculate the digest.
+    @param algo: The name of the algorithm to use to calculate the digest.
         Currently supported are md5, md5-sess, and sha.
-    @param pszMethod: The request method.
-    @param pszDigestUri: The request URI.
-    @param pszQop: The Quality-of-Protection value.
-    @param pszHEntity: The hash of the entity body or C{None} if C{pszQop} is
-        not C{'auth-int'}.
+    @param method: The request method.
+    @param digest_uri: The request URI.
+    @param qop: The Quality-of-Protection value.
+    @param entity: The entity body or C{None} if C{qop} is not C{'auth-int'}.
     @return: The hash of the A2 value for the calculation of the response
         digest.
     """
     m = algorithms[algo]()
-    m.update(pszMethod)
-    m.update(":")
-    m.update(pszDigestUri)
-    if pszQop == "auth-int":
-        m.update(":")
-        m.update(pszHEntity)
-    return m.digest().encode('hex')
+    m.update(method.encode())
+    m.update(b":")
+    m.update(digest_uri.encode())
+
+    if qop == "auth-int":
+        entity_data = entity.read()
+        m.update(b":")
+        m.update(hashlib.md5(entity_data).digest())
+
+    return m.digest().hex()
 
 
-def calcResponse(HA1, HA2, algo, pszNonce, pszNonceCount, pszCNonce, pszQop):
+def calcHEntity(entity: BinaryIO) -> str:
+    """
+    Compute H(entity) for auth-int qop.
+
+    @param entity: The entity body.
+    @return: The hash of the entity body.
+    """
+    return hashlib.md5(entity.read()).hexdigest()
+
+
+def calcResponse(
+    HA1: str,
+    HA2: str,
+    algo: str,
+    nonce: str,
+    nonce_count: str,
+    cnonce: str,
+    qop: str,
+) -> str:
     """
     Compute the digest for the given parameters.
 
     @param HA1: The H(A1) value, as computed by L{calcHA1}.
     @param HA2: The H(A2) value, as computed by L{calcHA2}.
-    @param pszNonce: The challenge nonce.
-    @param pszNonceCount: The (client) nonce count value for this response.
-    @param pszCNonce: The client nonce.
-    @param pszQop: The Quality-of-Protection value.
+    @param algo: The name of the algorithm to use to calculate the digest.
+        Currently supported are md5, md5-sess, and sha.
+    @param nonce: The challenge nonce.
+    @param nonce_count: The (client) nonce count value for this response.
+    @param cnonce: The client nonce.
+    @param qop: The Quality-of-Protection value.
     """
     m = algorithms[algo]()
-    m.update(HA1)
-    m.update(":")
-    m.update(pszNonce)
-    m.update(":")
-    if pszNonceCount and pszCNonce:
-        m.update(pszNonceCount)
-        m.update(":")
-        m.update(pszCNonce)
-        m.update(":")
-        m.update(pszQop)
-        m.update(":")
-    m.update(HA2)
-    respHash = m.digest().encode('hex')
-    return respHash
+    m.update(HA1.encode())
+    m.update(b":")
+    m.update(nonce.encode())
+    m.update(b":")
+
+    if nonce_count and cnonce:
+        m.update(nonce_count.encode())
+        m.update(b":")
+        m.update(cnonce.encode())
+        m.update(b":")
+        m.update(qop.encode())
+        m.update(b":")
+
+    m.update(HA2.encode())
+    resp_hash = m.digest()
+    return resp_hash.hex()
