24604733fe32d8620059c48cf37698f01dbdd948
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:47 +0545

    improve

--- __init__.py
+++ __init__.py
@@ -1,59 +1,62 @@
 # -*- coding: utf-8 -*-
 """
-/***************************************************************************
- gearthview
-                                 A QGIS plugin
- GEarth View
-                             -------------------
-        begin                : 2012-12-14
-        copyright            : (C) 2012 by geodrinx
-        email                : geodrinx@gmail.com
- ***************************************************************************/
+GEarth View - A QGIS plugin
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
+This script initializes the plugin, making it known to QGIS.
 """
 
-
-import sys 
-import os 
+import sys
+import os
 import site
 
+# Add the path to the extension libraries
 site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/ext-libs'))
 
 def name():
+    """
+    Return the plugin name
+    """
     return "GEarthView"
 
-
 def description():
-    return "GEarth View"
-
+    """
+    Return the plugin description
+    """
+    return "GEarth View - A plugin to display 3D globe views in QGIS"
 
 def version():
+    """
+    Return the plugin version
+    """
     return "Version 1.0.5"
 
-
 def icon():
-    return "icon.png"
-
+    """
+    Return the path to the plugin icon
+    """
+    return ":/plugins/gearthview/icon.png"
 
 def qgisMinimumVersion():
+    """
+    Return the minimum QGIS version required to run this plugin
+    """
     return "2.0"
 
 def author():
+    """
+    Return the plugin author
+    """
     return "geodrinx"
 
 def email():
+    """
+    Return the plugin author email
+    """
     return "geodrinx@gmail.com"
 
 def classFactory(iface):
-    # load gearthview class from file gearthview
-    from gearthview import gearthview
+    """
+    Load the gearthview class from the gearthview module
+    """
+    from .gearthview import gearthview
     return gearthview(iface)
