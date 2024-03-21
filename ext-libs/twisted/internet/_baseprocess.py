8034a9231c578b2c8b819a5876d62246c75ee450
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:54 +0545

    improve

--- ext-libs/twisted/internet/_baseprocess.py
+++ ext-libs/twisted/internet/_baseprocess.py
@@ -7,6 +7,7 @@ Cross-platform process-related functionality used by different
 L{IReactorProcess} implementations.
 """
 
+import typing
 from twisted.python.reflect import qual
 from twisted.python.deprecate import getWarningMethod
 from twisted.python.failure import Failure
@@ -17,16 +18,18 @@ _missingProcessExited = ("Since Twisted 8.2, IProcessProtocol.processExited "
                          "is required.  %s must implement it.")
 
 class BaseProcess(Ephemeral):
-    pid = None
-    status = None
-    lostProcess = 0
-    proto = None
+    """
+    A base class for managing a process.
+    """
+    pid: typing.Optional[int] = None
+    status: typing.Optional[int] = None
+    lostProcess: int = 0
+    proto: typing.Optional[typing.Any] = None
 
-    def __init__(self, protocol):
+    def __init__(self, protocol: typing.Any):
         self.proto = protocol
 
-
-    def _callProcessExited(self, reason):
+    def _callProcessExited(self, reason: typing.Any):
         default = object()
         processExited = getattr(self.proto, 'processExited', default)
         if processExited is default:
@@ -35,9 +38,9 @@ class BaseProcess(Ephemeral):
                 DeprecationWarning, stacklevel=0)
         else:
             processExited(Failure(reason))
+            self.proto = None  # Set proto to None here to avoid multiple calls in maybeCallProcessEnded
 
-
-    def processEnded(self, status):
+    def processEnded(self, status: int):
         """
         This is called when the child terminates.
         """
@@ -47,16 +50,18 @@ class BaseProcess(Ephemeral):
         self._callProcessExited(self._getReason(status))
         self.maybeCallProcessEnded()
 
-
     def maybeCallProcessEnded(self):
         """
         Call processEnded on protocol after final cleanup.
         """
         if self.proto is not None:
             reason = self._getReason(self.status)
-            proto = self.proto
-            self.proto = None
             try:
-                proto.processEnded(Failure(reason))
+                self.proto.processEnded(Failure(reason))
             except:
                 err(None, "unexpected error in processEnded")
+            finally:
+                self.proto = None
+
+    def __repr__(self) -> str:
+        return f'<BaseProcess pid={self.pid} status={self.status} lostProcess={self.lostProcess} proto={self.proto}>'
