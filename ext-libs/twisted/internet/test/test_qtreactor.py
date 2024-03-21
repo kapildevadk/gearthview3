d210c02b3a50f3192c29919bd22eb27d5166b63b
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:58 +0545

    improve

--- ext-libs/twisted/internet/test/test_qtreactor.py
+++ ext-libs/twisted/internet/test/test_qtreactor.py
@@ -2,11 +2,36 @@
 # See LICENSE for details.
 
 import sys
-
-from twisted.trial import unittest
+import unittest
 from twisted.python.runtime import platform
 from twisted.python.util import sibpath
 from twisted.internet.utils import getProcessOutputAndValue
+from twisted.plugins.twisted_qtstub import errorMessage
+from twisted.internet.qtreactor import Qtreactor  # Added for the test
+
+
+class QtreactorTestCase(unittest.TestCase):
+    """
+    Tests for L{twisted.internet.qtreactor}.
+    """
+
+    def test_importQtreactor(self):
+        """
+        Test that attempting to import L{twisted.internet.qtreactor} raises
+        an ImportError indicating that C{qtreactor} is no longer a part of
+        Twisted.
+        """
+        original_qtreactor = sys.modules.get('qtreactor')
+        try:
+            sys.modules['qtreactor'] = None
+            with self.assertRaises(ImportError) as cm:
+                import twisted.internet.qtreactor
+            self.assertEqual(str(cm.exception), f"No module named 'qtreactor'; "
+                                                f"twisted.internet.qtreactor "
+                                                f"has been removed. See {errorMessage} "
+                                                f"for details.")
+        finally:
+            sys.modules['qtreactor'] = original_qtreactor
 
 
 skipWindowsNopywin32 = None
@@ -14,22 +39,4 @@ if platform.isWindows():
     try:
         import win32process
     except ImportError:
-        skipWindowsNopywin32 = ("On windows, spawnProcess is not available "
-                                "in the absence of win32process.")
-
-class QtreactorTestCase(unittest.TestCase):
-    """
-    Tests for L{twisted.internet.qtreactor}.
-    """
-    def test_importQtreactor(self):
-        """
-        Attempting to import L{twisted.internet.qtreactor} should raise an
-        C{ImportError} indicating that C{qtreactor} is no longer a part of
-        Twisted.
-        """
-        sys.modules["qtreactor"] = None
-        from twisted.plugins.twisted_qtstub import errorMessage
-        try:
-            import twisted.internet.qtreactor
-        except ImportError, e:
-            self.assertEqual(str(e), errorMessage)
+        skipWindowsNopywin32 = ("On windows, spawnProcess
