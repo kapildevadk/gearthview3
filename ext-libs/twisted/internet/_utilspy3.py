aead66b3cae5fb8df6a4fa756214d5a4912b72d6
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:54 +0545

    improve

--- ext-libs/twisted/internet/_utilspy3.py
+++ ext-libs/twisted/internet/_utilspy3.py
@@ -8,52 +8,49 @@ Utility methods, ported to Python 3.
 
 from __future__ import division, absolute_import
 
-import sys, warnings
+import sys
+import warnings
 from functools import wraps
-
+from typing import Callable, List, Tuple, Any, Union, Optional
 from twisted.python.compat import reraise
 from twisted.internet import defer
 
-def _resetWarningFilters(passthrough, addedFilters):
-    for f in addedFilters:
-        try:
+def _resetWarningFilters(passthrough: Optional[Callable], added_filters: List[Tuple[str, int, str]]) -> Optional[Callable]:
+    for f in added_filters:
+        if len(warnings.filters) >= len(added_filters):
             warnings.filters.remove(f)
-        except ValueError:
-            pass
     return passthrough
 
+def runWithWarningsSuppressed(suppressed_warnings: List[Tuple[str, dict]], f: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
+    """Run the function `f`, but with some warnings suppressed.
 
-def runWithWarningsSuppressed(suppressedWarnings, f, *a, **kw):
-    """Run the function C{f}, but with some warnings suppressed.
-
-    @param suppressedWarnings: A list of arguments to pass to filterwarnings.
+    Args:
+        suppressed_warnings: A list of arguments to pass to filterwarnings.
                                Must be a sequence of 2-tuples (args, kwargs).
-    @param f: A callable, followed by its arguments and keyword arguments
+        f: A callable, followed by its arguments and keyword arguments
+
+    Returns:
+        The result of calling `f` with the given arguments and keyword arguments.
     """
-    for args, kwargs in suppressedWarnings:
-        warnings.filterwarnings(*args, **kwargs)
-    addedFilters = warnings.filters[:len(suppressedWarnings)]
-    try:
-        result = f(*a, **kw)
-    except:
-        exc_info = sys.exc_info()
-        _resetWarningFilters(None, addedFilters)
-        reraise(exc_info[1], exc_info[2])
-    else:
-        if isinstance(result, defer.Deferred):
-            result.addBoth(_resetWarningFilters, addedFilters)
+    with warnings.catch_warnings():
+        for args_, kwargs_ in suppressed_warnings:
+            warnings.filterwarnings(*args_, **kwargs_)
+        added_filters = warnings.filters[:len(suppressed_warnings)]
+        try:
+            result = f(*args, **kwargs)
+        except Exception as e:  # pylint: disable=broad-except
+            exc_info = sys.exc_info()
+            _resetWarningFilters(None, added_filters)
+            reraise(exc_info[1], exc_info[2], from_=e)
         else:
-            _resetWarningFilters(None, addedFilters)
-        return result
+            if isinstance(result, defer.Deferred):
+                result.addBoth(_resetWarningFilters, added_filters)
+            else:
+                _resetWarningFilters(None, added_filters)
+            return result
 
-
-def suppressWarnings(f, *suppressedWarnings):
+def suppressWarnings(f: Callable[..., Any], *suppressed_warnings: Tuple[str, dict]) -> Callable[..., Any]:
     """
-    Wrap C{f} in a callable which suppresses the indicated warnings before
-    invoking C{f} and unsuppresses them afterwards.  If f returns a Deferred,
-    warnings will remain suppressed until the Deferred fires.
-    """
-    @wraps(f)
-    def warningSuppressingWrapper(*a, **kw):
-        return runWithWarningsSuppressed(suppressedWarnings, f, *a, **kw)
-    return warningSuppressingWrapper
+    Wrap `f` in a callable which suppresses the indicated warnings before
+    invoking `f` and unsuppresses them afterwards.  If f returns a Deferred,
+    warnings will remain suppressed until the
