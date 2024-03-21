e1afae11f45044a5b7bae68260e0743775a8d8c5
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:45 +0545

    improve

--- ext-libs/twisted/conch/ui/__init__.py
+++ ext-libs/twisted/conch/ui/__init__.py
@@ -1,11 +1,13 @@
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
-
+# Copyright (c) 2003-2021 Twisted Matrix Laboratories.
+# All rights reserved.
 #
+# This software is licensed under the terms of the BSD license,
+# a copy of which is included in the LICENSE file in this
+# distribution.
+#
+# This module is part of Twisted, an event-driven networking engine.
+#
+# twisted.conch.ui is home to the UI elements for tkconch.
+#
+# Maintainer: Paul Swartz
 
-
-"""
-twisted.conch.ui is home to the UI elements for tkconch.
-
-Maintainer: Paul Swartz
-"""
