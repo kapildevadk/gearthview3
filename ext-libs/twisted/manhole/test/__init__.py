c3b040ebdb97955ada67f4ce5d017f542fa4de94
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:55 +0545

    improve

--- ext-libs/twisted/manhole/test/__init__.py
+++ ext-libs/twisted/manhole/test/__init__.py
@@ -4,3 +4,19 @@
 """
 Tests for L{twisted.manhole}.
 """
+
+import unittest
+from twisted.manhole import Manhole
+
+
+class ManholeTests(unittest.TestCase):
+    def setUp(self):
+        self.manhole = Manhole()
+
+    def test_something(self):
+        # Add your test case here
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
