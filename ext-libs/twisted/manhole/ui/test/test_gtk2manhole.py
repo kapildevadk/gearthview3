05e14b1c8ad5c6267bd81f6f051c0b598cdb1cd8
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:58 +0545

    improve

--- ext-libs/twisted/manhole/ui/test/test_gtk2manhole.py
+++ ext-libs/twisted/manhole/ui/test/test_gtk2manhole.py
@@ -3,46 +3,56 @@
 Tests for GTK2 GUI manhole.
 """
 
-skip = False
-
-try:
-    import pygtk
-    pygtk.require("2.0")
-except:
-    skip = "GTK 2.0 not available"
-else:
-    try:
-        import gtk
-    except ImportError:
-        skip = "GTK 2.0 not available"
-    except RuntimeError:
-        skip = "Old version of GTK 2.0 requires DISPLAY, and we don't have one."
-    else:
-        if gtk.gtk_version[0] == 1:
-            skip = "Requested GTK 2.0, but 1.0 was already imported."
-        else:
-            from twisted.manhole.ui.gtk2manhole import ConsoleInput
-
+import sys
+import pygtk
+import gtk
 from twisted.trial.unittest import TestCase
-
 from twisted.python.reflect import prefixedMethodNames
+from twisted.manhole.ui.gtk2manhole import ConsoleInput
+
+
+class GTK2RequirementError(Exception):
+    pass
+
+
+def check_gtk2_requirement():
+    try:
+        pygtk.require("2.0")
+    except:
+        raise GTK2RequirementError("GTK 2.0 not available")
+    try:
+        _ = gtk.gdk
+    except ImportError:
+        raise GTK2RequirementError("GTK 2.0 not available")
+    except RuntimeError:
+        raise GTK2RequirementError(
+            "Old version of GTK 2.0 requires DISPLAY, and we don't have one."
+        )
+    if gtk.gtk_version[0] == 1:
+        raise GTK2RequirementError(
+            "Requested GTK 2.0, but 1.0 was already imported."
+        )
+
 
 class ConsoleInputTests(TestCase):
     """
     Tests for L{ConsoleInput}.
     """
 
-    def test_reverseKeymap(self):
+    def setUp(self):
+        check_gtk2_requirement()
+        self.ci = ConsoleInput(None)
+
+    def test_reverse_keymap(self):
         """
         Verify that a L{ConsoleInput} has a reverse mapping of the keysym names
         it needs for event handling to their corresponding keysym.
         """
-        ci = ConsoleInput(None)
-        for eventName in prefixedMethodNames(ConsoleInput, 'key_'):
-            keysymName = eventName.split("_")[-1]
-            keysymValue = getattr(gtk.keysyms, keysymName)
-            self.assertEqual(ci.rkeymap[keysymValue], keysymName)
+        for event_name in prefixedMethodNames(ConsoleInput, "key_"):
+            keysym_name = event_name.split("_")[-1]
+            keysym_value = getattr(gtk.keysyms, keysym_name)
+            self.assertEqual(self.ci.rkeymap[keysym_value], keysym_name)
 
 
-    skip = skip
-
+if __name__ == "__main__":
+    if len(sys.
