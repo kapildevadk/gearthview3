1b94341ca46660f7a76a8333ee936c039805e518
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:44 +0545

    improve

--- ext-libs/zope/interface/tests/odd.py
+++ ext-libs/zope/interface/tests/odd.py
@@ -63,10 +63,7 @@ This is used for testing support for ExtensionClass in new interfaces.
   >>> int(C.__class__.__class__ is C.__class__)
   1
 
-$Id: odd.py 110699 2010-04-09 08:16:17Z regebro $
 """
-
-# class OddClass is an odd meta class
 
 class MetaMetaClass(type):
 
@@ -76,15 +73,14 @@ class MetaMetaClass(type):
         return type.__getattribute__(self, name)
     
 
-class MetaClass(object):
+class MetaClass(object, metaclass=MetaMetaClass):
     """Odd classes
     """
-    __metaclass__ = MetaMetaClass
 
-    def __init__(self, name, bases, dict):
+    def __init__(self, name, bases, namespace):
         self.__name__ = name
         self.__bases__ = bases
-        self.__dict__.update(dict)
+        self.__dict__.update(namespace)
 
     def __call__(self):
         return OddInstance(self)
@@ -102,30 +98,30 @@ class MetaClass(object):
 class OddInstance(object):
 
     def __init__(self, cls):
-        self.__dict__['__class__'] = cls
+        self.__class__ = cls
 
     def __getattribute__(self, name):
-        dict = object.__getattribute__(self, '__dict__')
+        dict_ = object.__getattribute__(self, '__dict__')
         if name == '__dict__':
-            return dict
-        v = dict.get(name, self)
-        if v is not self:
+            return dict_
+        v = dict_.get(name)
+        if v is not None:
             return v
-        return getattr(dict['__class__'], name)
+        return getattr(type(self), name)
 
-    def __setattr__(self, name, v):
-        self.__dict__[name] = v
+    def __setattr__(self, name, value):
+        dict_.__setitem__(self, name, value)
 
     def __delattr__(self, name):
-        del self.__dict__[name]
+        if name in dict_:
+            dict_.__delitem__(self, name)
+        else:
+            super().__delattr__(name)
 
     def __repr__(self):
         return "<odd %s instance at %s>" % (
             self.__class__.__name__, hex(id(self)))
-        
 
-
-# DocTest:
 if __name__ == "__main__":
     import doctest, __main__
     doctest.testmod(__main__, isprivate=lambda *a: False)
