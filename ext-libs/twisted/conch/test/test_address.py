9f4f7fa9c0b604a8951750382abd432cf1e15ca9
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:51 +0545

    improve

--- ext-libs/twisted/conch/test/test_address.py
+++ ext-libs/twisted/conch/test/test_address.py
@@ -5,6 +5,8 @@
 Tests for L{SSHTransportAddrress} in ssh/address.py
 """
 
+from typing import Any
+
 from twisted.trial import unittest
 from twisted.internet.address import IPv4Address
 from twisted.internet.test.test_address import AddressTestCaseMixin
@@ -12,38 +14,41 @@ from twisted.internet.test.test_address import AddressTestCaseMixin
 from twisted.conch.ssh.address import SSHTransportAddress
 
 
-
 class SSHTransportAddressTestCase(unittest.TestCase, AddressTestCaseMixin):
     """
-    L{twisted.conch.ssh.address.SSHTransportAddress} is what Conch transports
-    use to represent the other side of the SSH connection.  This tests the
-    basic functionality of that class (string representation, comparison, &c).
+    Tests for L{twisted.conch.ssh.address.SSHTransportAddress}.
     """
 
-
-    def _stringRepresentation(self, stringFunction):
+    def _stringRepresentation(self, stringFunction: Any) -> None:
         """
         The string representation of C{SSHTransportAddress} should be
         "SSHTransportAddress(<stringFunction on address>)".
         """
         addr = self.buildAddress()
-        stringValue = stringFunction(addr)
-        addressValue = stringFunction(addr.address)
-        self.assertEqual(stringValue,
-                         "SSHTransportAddress(%s)" % addressValue)
+        string_value = stringFunction(addr)
+        address_value = stringFunction(addr.address)
+        self.assertEqual(string_value, f"SSHTransportAddress({address_value})")
 
-
-    def buildAddress(self):
+    def buildAddress(self) -> SSHTransportAddress:
         """
         Create an arbitrary new C{SSHTransportAddress}.  A new instance is
         created for each call, but always for the same address.
         """
         return SSHTransportAddress(IPv4Address("TCP", "127.0.0.1", 22))
 
-
-    def buildDifferentAddress(self):
+    def buildDifferentAddress(self) -> SSHTransportAddress:
         """
         Like C{buildAddress}, but with a different fixed address.
         """
         return SSHTransportAddress(IPv4Address("TCP", "127.0.0.2", 22))
 
+    def test_equality(self) -> None:
+        """
+        Test the equality of two C{SSHTransportAddress} objects.
+        """
+        addr1 = self.buildAddress()
+        addr2 = self.buildAddress()
+        addr3 = self.buildDifferentAddress()
+        self.assertEqual(addr1, addr1)
+        self.assertEqual(addr1, addr2)
+        self.assertNotEqual(addr1, addr3)
