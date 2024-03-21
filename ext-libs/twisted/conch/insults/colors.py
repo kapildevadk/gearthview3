6a49ba2fabe159a6c232b6b24c0a3f513f132eb7
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:48 +0545

    improve

--- ext-libs/twisted/conch/insults/colors.py
+++ ext-libs/twisted/conch/insults/colors.py
@@ -1,29 +1,34 @@
 """
-You don't really want to use this module. Try helper.py instead.
+This module provides console text formatting options.
+For usage, please refer to the helper.py module.
 """
 
-CLEAR = 0
-BOLD = 1
-DIM = 2
-ITALIC = 3
-UNDERSCORE = 4
-BLINK_SLOW = 5
-BLINK_FAST = 6
-REVERSE = 7
-CONCEALED = 8
-FG_BLACK = 30
-FG_RED = 31
-FG_GREEN = 32
-FG_YELLOW = 33
-FG_BLUE = 34
-FG_MAGENTA = 35
-FG_CYAN = 36
-FG_WHITE = 37
-BG_BLACK = 40
-BG_RED = 41
-BG_GREEN = 42
-BG_YELLOW = 43
-BG_BLUE = 44
-BG_MAGENTA = 45
-BG_CYAN = 46
-BG_WHITE = 47
+class Format:
+    CLEAR = 0
+    BOLD = 1
+    DIM = 2
+    ITALIC = 3
+    UNDERSCORE = 4
+    BLINK_SLOW = 5
+    BLINK_FAST = 6
+    REVERSE = 7
+    CONCEALED = 8
+
+    class Colors:
+        FG_BLACK = 30
+        FG_RED = 31
+        FG_GREEN = 32
+        FG_YELLOW = 33
+        FG_BLUE = 34
+        FG_MAGENTA = 35
+        FG_CYAN = 36
+        FG_WHITE = 37
+
+        BG_BLACK = 40
+        BG_RED = 41
+        BG_GREEN = 42
+        BG_YELLOW = 43
+        BG_BLUE = 44
+        BG_MAGENTA = 45
+        BG_CYAN = 46
+        BG_WHITE = 47
