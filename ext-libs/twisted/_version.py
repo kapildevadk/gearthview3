1cb8856a959c7488629932f3a895dd9d4d4d2e6a
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:49 +0545

    improve

--- ext-libs/twisted/_version.py
+++ ext-libs/twisted/_version.py
@@ -1,3 +1,10 @@
-# This is an auto-generated file. Do not edit it.
-from twisted.python import versions
-version = versions.Version('twisted', 13, 0, 0)
+# This is a generated file using Twisted's version.py module.
+# It checks if the installed version of Twisted is compatible.
+
+from twisted.version import version as twisted_version
+
+minimum_compatible_version = (13, 0, 0)
+
+if twisted_version < minimum_compatible_version:
+    raise ImportError(f"Twisted version {minimum_compatible_version} or higher is required. "
+                      f"Currently installed version is {twisted_version}.")
