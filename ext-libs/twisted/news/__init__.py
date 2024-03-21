b3c8a4ebd5a192eed61f3d1e669dbc28b800cc31
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:00 +0545

    improve

--- ext-libs/twisted/news/__init__.py
+++ ext-libs/twisted/news/__init__.py
@@ -2,10 +2,7 @@
 # See LICENSE for details.
 
 """
-
 Twisted News: an NNTP-based news service.
-
 """
 
-from twisted.news._version import version
-__version__ = version.short()
+from twisted.news._version import __version__ as version
