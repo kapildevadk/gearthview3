cee004d1efa43b872ff852667852af4a88e92695
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:45 +0545

    improve

--- ext-libs/twisted/conch/ssh/__init__.py
+++ ext-libs/twisted/conch/ssh/__init__.py
@@ -1,10 +1,6 @@
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-# 
-
-"""
-An SSHv2 implementation for Twisted.  Part of the Twisted.Conch package.
-
-Maintainer: Paul Swartz
-"""
+# Copyright (c) 2023 <Your Name>
+#
+# This file is part of the Twisted project.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root of the source distribution.
