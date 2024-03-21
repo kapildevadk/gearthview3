87512c17a8371a0f33795efca6b6663f56b964ef
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:40 +0545

    improve

--- ext-libs/twisted/web/domhelpers.py
+++ ext-libs/twisted/web/domhelpers.py
@@ -6,9 +6,9 @@
 A library for performing interesting tasks with DOM objects.
 """
 
-import StringIO
+import string
+from typing import Any, Dict, List, Optional, Union
 
-from twisted.web import microdom
 from twisted.web.microdom import getElementsByTagName, escape, unescape
 
 
@@ -16,253 +16,126 @@ class NodeLookupError(Exception):
     pass
 
 
-def substitute(request, node, subs):
+def substitute(node: microdom.Node, subs: Dict[str, str]) -> None:
     """
     Look through the given node's children for strings, and
     attempt to do string substitution with the given parameter.
     """
     for child in node.childNodes:
-        if hasattr(child, 'nodeValue') and child.nodeValue:
-            child.replaceData(0, len(child.nodeValue), child.nodeValue % subs)
-        substitute(request, child, subs)
+        if hasattr(child, "nodeValue") and child.nodeValue:
+            child.replaceData(
+                0, len(child.nodeValue), child.nodeValue % subs
+            )
+        substitute(child, subs)
 
-def _get(node, nodeId, nodeAttrs=('id','class','model','pattern')):
-    """
-    (internal) Get a node with the specified C{nodeId} as any of the C{class},
-    C{id} or C{pattern} attributes.
-    """
 
-    if hasattr(node, 'hasAttributes') and node.hasAttributes():
-        for nodeAttr in nodeAttrs:
-            if (str (node.getAttribute(nodeAttr)) == nodeId):
+def _get_node(
+    node: microdom.Node, node_id: str, node_attrs: Tuple[str, ...]
+) -> Optional[microdom.Node]:
+    """
+    (internal) Get a node with the specified node_id as any of the class,
+    id or pattern attributes.
+    """
+    if hasattr(node, "hasAttributes") and node.hasAttributes():
+        for node_attr in node_attrs:
+            if str(node.getAttribute(node_attr)) == node_id:
                 return node
     if node.hasChildNodes():
-        if hasattr(node.childNodes, 'length'):
+        if hasattr(node.childNodes, "length"):
             length = node.childNodes.length
         else:
             length = len(node.childNodes)
-        for childNum in range(length):
-            result = _get(node.childNodes[childNum], nodeId)
-            if result: return result
+        for child_num in range(length):
+            result = _get_node(node.childNodes[childNum], node_id, node_attrs)
+            if result:
+                return result
+    return None
 
-def get(node, nodeId):
+
+def get_node(node: microdom.Node, node_id: str) -> microdom.Node:
     """
-    Get a node with the specified C{nodeId} as any of the C{class},
-    C{id} or C{pattern} attributes. If there is no such node, raise
+    Get a node with the specified node_id as any of the class,
+    id or pattern attributes. If there is no such node, raise
     L{NodeLookupError}.
     """
-    result = _get(node, nodeId)
-    if result: return result
-    raise NodeLookupError, nodeId
+    result = _get_node(node, node_id, ("id", "class", "model", "pattern"))
+    if result:
+        return result
+    raise NodeLookupError(node_id)
 
-def getIfExists(node, nodeId):
+
+def get_node_if_exists(node: microdom.Node, node_id: str) -> Optional[microdom.Node]:
     """
-    Get a node with the specified C{nodeId} as any of the C{class},
-    C{id} or C{pattern} attributes.  If there is no such node, return
+    Get a node with the specified node_id as any of the class,
+    id or pattern attributes.  If there is no such node, return
     C{None}.
     """
-    return _get(node, nodeId)
+    return _get_node(node, node_id, ("id", "class", "model", "pattern"))
 
-def getAndClear(node, nodeId):
-    """Get a node with the specified C{nodeId} as any of the C{class},
-    C{id} or C{pattern} attributes. If there is no such node, raise
+
+def get_and_clear_node(node: microdom.Node, node_id: str) -> Optional[microdom.Node]:
+    """Get a node with the specified node_id as any of the class,
+    id or pattern attributes. If there is no such node, raise
     L{NodeLookupError}. Remove all child nodes before returning.
     """
-    result = get(node, nodeId)
+    result = get_node(node, node_id)
     if result:
-        clearNode(result)
+        clear_node(result)
     return result
 
-def clearNode(node):
+
+def clear_node(node: microdom.Node) -> None:
     """
     Remove all children from the given node.
     """
     node.childNodes[:] = []
 
-def locateNodes(nodeList, key, value, noNesting=1):
+
+def locate_nodes(
+    node_list: Union[microdom.Node, List[microdom.Node]],
+    key: str,
+    value: str,
+    no_nesting: bool = True,
+) -> List[microdom.Node]:
     """
     Find subnodes in the given node where the given attribute
     has the given value.
     """
-    returnList = []
-    if not isinstance(nodeList, type([])):
-        return locateNodes(nodeList.childNodes, key, value, noNesting)
-    for childNode in nodeList:
-        if not hasattr(childNode, 'getAttribute'):
+    return_list = []
+    if not isinstance(node_list, list):
+        return locate_nodes(node_list.childNodes, key, value, no_nesting)
+    for child_node in node_list:
+        if not hasattr(child_node, "getAttribute"):
             continue
-        if str(childNode.getAttribute(key)) == value:
-            returnList.append(childNode)
-            if noNesting:
+        if str(child_node.getAttribute(key)) == value:
+            return_list.append(child_node)
+            if no_nesting:
                 continue
-        returnList.extend(locateNodes(childNode, key, value, noNesting))
-    return returnList
+        return_list.extend(
+            locate_nodes(child_node, key, value, no_nesting)
+        )
+    return return_list
 
-def superSetAttribute(node, key, value):
-    if not hasattr(node, 'setAttribute'): return
+
+def super_set_attribute(
+    node: microdom.Node, key: str, value: str
+) -> None:
+    if not hasattr(node, "setAttribute"):
+        return
     node.setAttribute(key, value)
     if node.hasChildNodes():
         for child in node.childNodes:
-            superSetAttribute(child, key, value)
+            super_set_attribute(child, key, value)
 
-def superPrependAttribute(node, key, value):
-    if not hasattr(node, 'setAttribute'): return
+
+def super_prepend_attribute(
+    node: microdom.Node, key: str, value: str
+) -> None:
+    if not hasattr(node, "setAttribute"):
+        return
     old = node.getAttribute(key)
     if old:
-        node.setAttribute(key, value+'/'+old)
+        node.setAttribute(key, value + "/" + old)
     else:
         node.setAttribute(key, value)
-    if node.hasChildNodes():
-        for child in node.childNodes:
-            superPrependAttribute(child, key, value)
-
-def superAppendAttribute(node, key, value):
-    if not hasattr(node, 'setAttribute'): return
-    old = node.getAttribute(key)
-    if old:
-        node.setAttribute(key, old + '/' + value)
-    else:
-        node.setAttribute(key, value)
-    if node.hasChildNodes():
-        for child in node.childNodes:
-            superAppendAttribute(child, key, value)
-
-def gatherTextNodes(iNode, dounescape=0, joinWith=""):
-    """Visit each child node and collect its text data, if any, into a string.
-For example::
-    >>> doc=microdom.parseString('<a>1<b>2<c>3</c>4</b></a>')
-    >>> gatherTextNodes(doc.documentElement)
-    '1234'
-With dounescape=1, also convert entities back into normal characters.
-@return: the gathered nodes as a single string
-@rtype: str
-"""
-    gathered=[]
-    gathered_append=gathered.append
-    slice=[iNode]
-    while len(slice)>0:
-        c=slice.pop(0)
-        if hasattr(c, 'nodeValue') and c.nodeValue is not None:
-            if dounescape:
-                val=unescape(c.nodeValue)
-            else:
-                val=c.nodeValue
-            gathered_append(val)
-        slice[:0]=c.childNodes
-    return joinWith.join(gathered)
-
-class RawText(microdom.Text):
-    """This is an evil and horrible speed hack. Basically, if you have a big
-    chunk of XML that you want to insert into the DOM, but you don't want to
-    incur the cost of parsing it, you can construct one of these and insert it
-    into the DOM. This will most certainly only work with microdom as the API
-    for converting nodes to xml is different in every DOM implementation.
-
-    This could be improved by making this class a Lazy parser, so if you
-    inserted this into the DOM and then later actually tried to mutate this
-    node, it would be parsed then.
-    """
-
-    def writexml(self, writer, indent="", addindent="", newl="", strip=0, nsprefixes=None, namespace=None):
-        writer.write("%s%s%s" % (indent, self.data, newl))
-
-def findNodes(parent, matcher, accum=None):
-    if accum is None:
-        accum = []
-    if not parent.hasChildNodes():
-        return accum
-    for child in parent.childNodes:
-        # print child, child.nodeType, child.nodeName
-        if matcher(child):
-            accum.append(child)
-        findNodes(child, matcher, accum)
-    return accum
-
-
-def findNodesShallowOnMatch(parent, matcher, recurseMatcher, accum=None):
-    if accum is None:
-        accum = []
-    if not parent.hasChildNodes():
-        return accum
-    for child in parent.childNodes:
-        # print child, child.nodeType, child.nodeName
-        if matcher(child):
-            accum.append(child)
-        if recurseMatcher(child):
-            findNodesShallowOnMatch(child, matcher, recurseMatcher, accum)
-    return accum
-
-def findNodesShallow(parent, matcher, accum=None):
-    if accum is None:
-        accum = []
-    if not parent.hasChildNodes():
-        return accum
-    for child in parent.childNodes:
-        if matcher(child):
-            accum.append(child)
-        else:
-            findNodes(child, matcher, accum)
-    return accum
-
-
-def findElementsWithAttributeShallow(parent, attribute):
-    """
-    Return an iterable of the elements which are direct children of C{parent}
-    and which have the C{attribute} attribute.
-    """
-    return findNodesShallow(parent,
-        lambda n: getattr(n, 'tagName', None) is not None and
-            n.hasAttribute(attribute))
-
-
-def findElements(parent, matcher):
-    """
-    Return an iterable of the elements which are children of C{parent} for
-    which the predicate C{matcher} returns true.
-    """
-    return findNodes(
-        parent,
-        lambda n, matcher=matcher: getattr(n, 'tagName', None) is not None and
-                                   matcher(n))
-
-def findElementsWithAttribute(parent, attribute, value=None):
-    if value:
-        return findElements(
-            parent,
-            lambda n, attribute=attribute, value=value:
-              n.hasAttribute(attribute) and n.getAttribute(attribute) == value)
-    else:
-        return findElements(
-            parent,
-            lambda n, attribute=attribute: n.hasAttribute(attribute))
-
-
-def findNodesNamed(parent, name):
-    return findNodes(parent, lambda n, name=name: n.nodeName == name)
-
-
-def writeNodeData(node, oldio):
-    for subnode in node.childNodes:
-        if hasattr(subnode, 'data'):
-            oldio.write(subnode.data)
-        else:
-            writeNodeData(subnode, oldio)
-
-
-def getNodeText(node):
-    oldio = StringIO.StringIO()
-    writeNodeData(node, oldio)
-    return oldio.getvalue()
-
-
-def getParents(node):
-    l = []
-    while node:
-        l.append(node)
-        node = node.parentNode
-    return l
-
-def namedChildren(parent, nodeName):
-    """namedChildren(parent, nodeName) -> children (not descendants) of parent
-    that have tagName == nodeName
-    """
-    return [n for n in parent.childNodes if getattr(n, 'tagName', '')==nodeName]
+    if
