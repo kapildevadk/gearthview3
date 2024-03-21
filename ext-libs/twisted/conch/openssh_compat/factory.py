f25ca1a5ec22745e289b2e777128202a8d06d3a7
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:54 +0545

    improve

--- ext-libs/twisted/conch/openssh_compat/factory.py
+++ ext-libs/twisted/conch/openssh_compat/factory.py
@@ -7,67 +7,71 @@ Factory for reading openssh configuration files: public keys, private keys, and
 moduli file.
 """
 
-import os, errno
-
+import os
+import errno
+from contextlib import suppress
 from twisted.python import log
 from twisted.python.util import runAsEffectiveUser
-
 from twisted.conch.ssh import keys, factory, common
 from twisted.conch.openssh_compat import primes
 
 
-
 class OpenSSHFactory(factory.SSHFactory):
+    """
+    A factory for creating SSH servers using OpenSSH-style keys and configuration.
+    """
     dataRoot = '/usr/local/etc'
-    moduliRoot = '/usr/local/etc' # for openbsd which puts moduli in a different
-                                  # directory from keys
-
+    dataSubdir = 'ssh'
+    moduliRoot = '/usr/local/etc'
 
     def getPublicKeys(self):
         """
         Return the server public keys.
         """
-        ks = {}
-        for filename in os.listdir(self.dataRoot):
-            if filename[:9] == 'ssh_host_' and filename[-8:]=='_key.pub':
+        keys_by_type = {}
+        for filename in os.listdir(os.path.join(self.dataRoot, self.dataSubdir)):
+            if filename.startswith('ssh_host_') and filename.endswith('.pub'):
+                filepath = os.path.join(self.dataRoot, self.dataSubdir, filename)
+                if not os.path.exists(filepath):
+                    log.msg(f'File does not exist: {filepath}')
+                    continue
                 try:
-                    k = keys.Key.fromFile(
-                        os.path.join(self.dataRoot, filename))
-                    t = common.getNS(k.blob())[0]
-                    ks[t] = k
-                except Exception, e:
-                    log.msg('bad public key file %s: %s' % (filename, e))
-        return ks
-
+                    key = keys.Key.fromFile(filepath)
+                    key_type = common.getNS(key.blob())[0]
+                    keys_by_type[key_type] = key
+                except Exception as e:
+                    log.msg(f'Error reading public key file {filename}: {e}')
+        return keys_by_type
 
     def getPrivateKeys(self):
         """
         Return the server private keys.
         """
-        privateKeys = {}
-        for filename in os.listdir(self.dataRoot):
-            if filename[:9] == 'ssh_host_' and filename[-4:]=='_key':
-                fullPath = os.path.join(self.dataRoot, filename)
+        private_keys_by_type = {}
+        for filename in os.listdir(os.path.join(self.dataRoot, self.dataSubdir)):
+            if filename.startswith('ssh_host_') and filename.endswith('_key'):
+                filepath = os.path.join(self.dataRoot, self.dataSubdir, filename)
+                if not os.path.exists(filepath):
+                    log.msg(f'File does not exist: {filepath}')
+                    continue
                 try:
-                    key = keys.Key.fromFile(fullPath)
-                except IOError, e:
+                    with open(filepath, 'r') as f:
+                        key_data = f.read()
+                except IOError as e:
                     if e.errno == errno.EACCES:
                         # Not allowed, let's switch to root
-                        key = runAsEffectiveUser(0, 0, keys.Key.fromFile, fullPath)
-                        keyType = keys.objectType(key.keyObject)
-                        privateKeys[keyType] = key
+                        with suppress(IOError):
+                            key = runAsEffectiveUser(0, 0, keys.Key.fromFile, filepath)
+                            key_type = keys.objectType(key.keyObject)
+                            private_keys_by_type[key_type] = key
                     else:
                         raise
-                except Exception, e:
-                    log.msg('bad private key file %s: %s' % (filename, e))
+                except Exception as e:
+                    log.msg(f'Error reading private key file {filename}: {e}')
                 else:
-                    keyType = keys.objectType(key.keyObject)
-                    privateKeys[keyType] = key
-        return privateKeys
-
-
-    def getPrimes(self):
-        try:
-            return primes.parseModuliFile(self.moduliRoot+'/moduli')
-        except IOError:
-            return None
+                    try:
+                        key = keys.Key.fromData(key_data)
+                    except Exception as e:
+                        log.msg(f'Error parsing private key data: {e}')
+                    else:
+                        key_type = keys.objectType(
