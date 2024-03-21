dacdc4370932f0f4655c8ca36acc6fb4255b9e8f
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:55 +0545

    improve

--- ext-libs/twisted/lore/htmlbook.py
+++ ext-libs/twisted/lore/htmlbook.py
@@ -3,47 +3,50 @@
 
 from twisted.python.compat import execfile
 
-
-def getNumber(filename):
-    return None
-
-def getReference(filename):
-    return None
-
 class Book:
+    """
+    A class to represent a book with chapters and an index file.
+    """
 
-    def __init__(self, filename):
+    def __init__(self, filename=None):
+        """
+        Initialize a new Book object.
+
+        :param filename: The filename of the book configuration file.
+        """
         self.chapters = []
-        self.indexFilename = None
-
-        global Chapter
-        Chapter = self.Chapter
-        global getNumber
-        getNumber = self.getNumber
-        global getReference
-        getReference = self.getNumber
-        global Index
-        Index = self.Index
+        self.index_filename = None
 
         if filename:
             execfile(filename, globals())
 
-    def getFiles(self):
+    @property
+    def get_files(self):
+        """
+        A read-only property to get the list of chapter filenames.
+
+        :return: A list of chapter filenames.
+        """
         return [c[0] for c in self.chapters]
 
-    def getNumber(self, filename):
-        for c in self.chapters:
-            if c[0] == filename:
-                return c[1]
+    def get_number(self, filename):
+        """
+        Get the number associated with a chapter filename.
+
+        :param filename: The filename of the chapter.
+        :return: The number associated with the chapter or None if not found.
+        """
+        for chapter in self.chapters:
+            if chapter[0] == filename:
+                return chapter[1]
         return None
 
-    def getIndexFilename(self):
-        return self.indexFilename
+    def get_index_filename(self):
+        """
+        A read-only property to get the index filename.
 
-    def Chapter(self, filename, number):
-        self.chapters.append((filename, number))
+        :return: The index filename or None if not set.
+        """
+        return self.index_filename
 
-    def Index(self, filename):
-        self.indexFilename = filename
-
-#_book = Book(None)
+   
