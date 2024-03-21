9816419de3346dbca73fd88bf56a30784cbd2030
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:56 +0545

    improve

--- ext-libs/twisted/plugins/twisted_web.py
+++ ext-libs/twisted/plugins/twisted_web.py
@@ -3,9 +3,17 @@
 
 from twisted.application.service import ServiceMaker
 
-TwistedWeb = ServiceMaker(
+service_maker = ServiceMaker(
     "Twisted Web",
     "twisted.web.tap",
     ("A general-purpose web server which can serve from a "
      "filesystem or application resource."),
-    "web")
+    "web"
+)
+
+service_maker.description = "A highly flexible and scalable web server"
+service_maker.options.addOption(
+    "-p", "--port", type="int", default=8080,
+    help="Port number for the web server to listen on"
+)
+
