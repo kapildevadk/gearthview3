3f717e248d24a39caa96ee6b3873944fd7438b02
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:42 +0545

    improve

--- ext-libs/twisted/conch/openssh_compat/__init__.py
+++ ext-libs/twisted/conch/openssh_compat/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-# 
-
 """
 Support for OpenSSH configuration files.
 
 Maintainer: Paul Swartz
 """
+
+import re
+from twisted.python.util import LineIter
 
