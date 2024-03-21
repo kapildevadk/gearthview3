84bff3bada99e9f68fdbffe8b11238a420a3b0e1
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:47 +0545

    improve

--- ext-libs/twisted/words/test/test_jabberxmppstringprep.py
+++ ext-libs/twisted/words/test/test_jabberxmppstringprep.py
@@ -1,92 +1,104 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-from twisted.trial import unittest
-
-from twisted.words.protocols.jabber.xmpp_stringprep import nodeprep, resourceprep, nameprep, crippled
+import unittest
+from twisted.words.protocols.jabber.xmpp_stringprep import (
+    nodeprep,
+    resourceprep,
+    nameprep,
+    crippled,
+)
 
 class XMPPStringPrepTest(unittest.TestCase):
     """
-
-    The nodeprep stringprep profile is similar to the resourceprep profile,
-    but does an extra mapping of characters (table B.2) and disallows
-    more characters (table C.1.1 and eight extra punctuation characters).
-    Due to this similarity, the resourceprep tests are more extensive, and
-    the nodeprep tests only address the mappings additional restrictions.
-
-    The nameprep profile is nearly identical to the nameprep implementation in
-    L{encodings.idna}, but that implementation assumes the C{UseSTD4ASCIIRules}
-    flag to be false. This implementation assumes it to be true, and restricts
-    the allowed set of characters.  The tests here only check for the
-    differences.
-    
+    Tests for XMPP stringprep profiles.
     """
 
-    def testResourcePrep(self):
-        self.assertEqual(resourceprep.prepare(u'resource'), u'resource')
-        self.assertNotEquals(resourceprep.prepare(u'Resource'), u'resource')
-        self.assertEqual(resourceprep.prepare(u' '), u' ')
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the test case.
+        """
+        super().__init__(*args, **kwargs)
+        self.no_resourceprep_tests = crippled
 
-        if crippled:
+    def test_resourceprep(self):
+        """
+        Test the resourceprep stringprep profile.
+        """
+        self.assertEqual(resourceprep.prepare(u"resource"), u"resource")
+        self.assertNotEqual(resourceprep.prepare(u"Resource"), u"resource")
+        self.assertEqual(resourceprep.prepare(u" "), u" ")
+
+        if self.no_resourceprep_tests:
             return
 
-        self.assertEqual(resourceprep.prepare(u'Henry \u2163'), u'Henry IV')
-        self.assertEqual(resourceprep.prepare(u'foo\xad\u034f\u1806\u180b'
-                                               u'bar\u200b\u2060'
-                                               u'baz\ufe00\ufe08\ufe0f\ufeff'),
-                          u'foobarbaz')
-        self.assertEqual(resourceprep.prepare(u'\u00a0'), u' ')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u1680')
-        self.assertEqual(resourceprep.prepare(u'\u2000'), u' ')
-        self.assertEqual(resourceprep.prepare(u'\u200b'), u'')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u0010\u007f')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u0085')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u180e')
-        self.assertEqual(resourceprep.prepare(u'\ufeff'), u'')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\uf123')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U000f1234')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U0010f234')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U0008fffe')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U0010ffff')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\udf42')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\ufffd')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u2ff5')
-        self.assertEqual(resourceprep.prepare(u'\u0341'), u'\u0301')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u200e')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u202a')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U000e0001')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U000e0042')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'foo\u05bebar')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'foo\ufd50bar')
-        #self.assertEqual(resourceprep.prepare(u'foo\ufb38bar'),
-        #                  u'foo\u064ebar')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\u06271')
-        self.assertEqual(resourceprep.prepare(u'\u06271\u0628'),
-                          u'\u06271\u0628')
-        self.assertRaises(UnicodeError, resourceprep.prepare, u'\U000e0002')
+        resourceprep_extra_tests = [
+            (u"Henry \u2163", u"Henry IV"),
+            (u"foo\xad\u034f\u1806\u180b bar\u200b\u2060 baz\ufe00\ufe08\ufe0f\ufeff", u"foobarbaz"),
+            (u"\u00a0", u" "),
+            (u"\u1680", UnicodeError),
+            (u"\u2000", u" "),
+            (u"\u200b", u""),
+            (u"\u0010\u007f", UnicodeError),
+            (u"\u0085", UnicodeError),
+            (u"\u180e", UnicodeError),
+            (u"\ufeff", u""),
+            (u"\uf123", UnicodeError),
+            (u"\U000f1234", UnicodeError),
+            (u"\U0010f234", UnicodeError),
+            (u"\U0008fffe", UnicodeError),
+            (u"\U0010ffff", UnicodeError),
+            (u"\udf42", UnicodeError),
+            (u"\ufffd", UnicodeError),
+            (u"\u2ff5", UnicodeError),
+            (u"\u0341", u"\u0301"),
+            (u"\u200e", UnicodeError),
+            (u"\u202a", UnicodeError),
+            (u"\U000e0001", UnicodeError),
+            (u"\U000e0042", UnicodeError),
+            (u"foo\u05bebar", UnicodeError),
+            (u"foo\ufd50bar", UnicodeError),
+            # (u"foo\ufb38bar", u"foo\u064ebar"),
+            (u"\u06271", UnicodeError),
+            (u"\u06271\u0628", u"\u06271\u0628"),
+            (u"\U000e0002", UnicodeError),
+        ]
 
-    def testNodePrep(self):
-        self.assertEqual(nodeprep.prepare(u'user'), u'user')
-        self.assertEqual(nodeprep.prepare(u'User'), u'user')
-        self.assertRaises(UnicodeError, nodeprep.prepare, u'us&er')
+        for input, expected in resourceprep_extra_tests:
+            with self.subTest(input=input):
+                if isinstance(expected, str):
+                    self.assertEqual(resourceprep.prepare(input), expected)
+                else:
+                    self.assertRaises(UnicodeError, resourceprep.prepare, input)
 
+    @unittest.skipIf(crippled, "Skipping nodeprep tests")
+    def test_nodeprep(self):
+        """
+        Test the nodeprep stringprep profile.
+        """
+        self.assertEqual(nodeprep.prepare(u"user"), u"user")
+        self.assertEqual(nodeprep.prepare(u"User"), u"user")
+        self.assertRaises(UnicodeError, nodeprep.prepare, u"us&er")
 
-    def test_nodeprepUnassignedInUnicode32(self):
+    def test_nodeprep_unassigned_unicode_3_2(self):
         """
         Make sure unassigned code points from Unicode 3.2 are rejected.
         """
-        self.assertRaises(UnicodeError, nodeprep.prepare, u'\u1d39')
+        self.assertRaises(UnicodeError, nodeprep.prepare, u"\u1d39")
 
+    @unittest.skipIf(crippled, "Skipping nameprep tests")
+    def test_nameprep(self):
+        """
+        Test the nameprep stringprep profile.
+        """
+        self.assertEqual(nameprep.prepare(u"example.com"), u"example.com")
+        self.assertEqual(nameprep.prepare(u"Example.com"), u"example.com")
+        self.assertRaises(UnicodeError, nameprep.prepare, u"ex@mple.com")
+        self.assertRaises(UnicodeError, nameprep.prepare, u"-example.com")
+        self.assertRaises(UnicodeError, nameprep.prepare, u"example-.com")
 
-    def testNamePrep(self):
-        self.assertEqual(nameprep.prepare(u'example.com'), u'example.com')
-        self.assertEqual(nameprep.prepare(u'Example.com'), u'example.com')
-        self.assertRaises(UnicodeError, nameprep.prepare, u'ex@mple.com')
-        self.assertRaises(UnicodeError, nameprep.prepare, u'-example.com')
-        self.assertRaises(UnicodeError, nameprep.prepare, u'example-.com')
+        nameprep_extra_tests = [
+            (u"stra\u00dfe.example.com", u"strasse.example.com"),
+        ]
 
-        if crippled:
-            return
-
-        self.assertEqual(nameprep.prepare(u'stra\u00dfe.example.com'),
-                          u'strasse.example.com')
+        for input, expected in nameprep_extra_
