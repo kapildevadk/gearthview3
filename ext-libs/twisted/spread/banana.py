937edd5abc5c27889d30b07373f1d18b55405d09
Kapil <kapildevadk@gmail.com>
2024-03-21 18:02:42 +0545

    improve

--- ext-libs/twisted/spread/banana.py
+++ ext-libs/twisted/spread/banana.py
@@ -12,7 +12,8 @@ for more details.
 @author: Glyph Lefkowitz
 """
 
-import copy, cStringIO, struct
+import struct
+from typing import Any, Dict, List, Optional
 
 from twisted.internet import protocol
 from twisted.persisted import styles
@@ -21,17 +22,16 @@ from twisted.python import log
 class BananaError(Exception):
     pass
 
-def int2b128(integer, stream):
+def int2b128(integer: int, stream: str) -> None:
     if integer == 0:
-        stream(chr(0))
+        stream += chr(0)
         return
     assert integer > 0, "can only encode positive integers"
     while integer:
-        stream(chr(integer & 0x7f))
-        integer = integer >> 7
+        stream += chr(integer & 0x7f)
+        integer >>= 7
 
-
-def b1282int(st):
+def b1282int(st: str) -> int:
     """
     Convert an integer represented as a base 128 string into an C{int} or
     C{long}.
@@ -50,7 +50,6 @@ def b1282int(st):
         e <<= 7
     return i
 
-
 # delimiter characters.
 LIST     = chr(0x80)
 INT      = chr(0x81)
@@ -65,7 +64,7 @@ VOCAB    = chr(0x87)
 
 HIGH_BIT_SET = chr(0x80)
 
-def setPrefixLimit(limit):
+def setPrefixLimit(limit: int) -> None:
     """
     Set the limit on the prefix length for all Banana connections
     established after this call.
@@ -84,12 +83,51 @@ setPrefixLimit(64)
 SIZE_LIMIT = 640 * 1024   # 640k is all you'll ever need :-)
 
 class Banana(protocol.Protocol, styles.Ephemeral):
+    """
+    A protocol for communicating using s-expressions.
+
+    @ivar knownDialects: A list of dialects that this protocol understands.
+    @type knownDialects: C{list} of C{str}
+
+    @ivar prefixLimit: The maximum length of the prefix allowed for decoding.
+    @type prefixLimit: C{int}
+
+    @ivar sizeLimit: The maximum size of a message allowed for decoding.
+    @type sizeLimit: C{int}
+
+    @ivar currentDialect: The current dialect being used for communication.
+    @type currentDialect: C{str}
+
+    @ivar listStack: A stack of lists being decoded.
+    @type listStack: C{list} of C{tuple} of C{int} and C{list}
+
+    @ivar buffer: A buffer of data waiting to be decoded.
+    @type buffer: C{str}
+
+    @ivar outgoingSymbols: A dictionary of outgoing symbols and their IDs.
+    @type outgoingSymbols: C{dict} of C{str} and C{int}
+
+    @ivar outgoingSymbolCount: The next available ID for an outgoing symbol.
+    @type outgoingSymbolCount: C{int}
+
+    @ivar isClient: Whether this protocol is a client or not.
+    @type isClient: C{bool}
+
+    @ivar incomingVocabulary: A dictionary of incoming symbol IDs and their names.
+    @type incomingVocabulary: C{dict} of C{int} and C{str}
+    """
     knownDialects = ["pb", "none"]
 
-    prefixLimit = None
+    prefixLimit: Optional[int] = None
     sizeLimit = SIZE_LIMIT
 
-    def setPrefixLimit(self, limit):
+    def __init__(self, isClient: bool = True) -> None:
+        self.listStack = []
+        self.outgoingSymbols = copy.copy(self.outgoingVocabulary)
+        self.outgoingSymbolCount = 0
+        self.isClient = isClient
+
+    def setPrefixLimit(self, limit: int) -> None:
         """
         Set the prefix limit for decoding done by this protocol instance.
 
@@ -99,260 +137,4 @@ class Banana(protocol.Protocol, styles.Ephemeral):
         self._smallestLongInt = -2 ** (limit * 7) + 1
         self._smallestInt = -2 ** 31
         self._largestInt = 2 ** 31 - 1
-        self._largestLongInt = 2 ** (limit * 7) - 1
-
-
-    def connectionReady(self):
-        """Surrogate for connectionMade
-        Called after protocol negotiation.
-        """
-
-    def _selectDialect(self, dialect):
-        self.currentDialect = dialect
-        self.connectionReady()
-
-    def callExpressionReceived(self, obj):
-        if self.currentDialect:
-            self.expressionReceived(obj)
-        else:
-            # this is the first message we've received
-            if self.isClient:
-                # if I'm a client I have to respond
-                for serverVer in obj:
-                    if serverVer in self.knownDialects:
-                        self.sendEncoded(serverVer)
-                        self._selectDialect(serverVer)
-                        break
-                else:
-                    # I can't speak any of those dialects.
-                    log.msg("The client doesn't speak any of the protocols "
-                            "offered by the server: disconnecting.")
-                    self.transport.loseConnection()
-            else:
-                if obj in self.knownDialects:
-                    self._selectDialect(obj)
-                else:
-                    # the client just selected a protocol that I did not suggest.
-                    log.msg("The client selected a protocol the server didn't "
-                            "suggest and doesn't know: disconnecting.")
-                    self.transport.loseConnection()
-
-
-    def connectionMade(self):
-        self.setPrefixLimit(_PREFIX_LIMIT)
-        self.currentDialect = None
-        if not self.isClient:
-            self.sendEncoded(self.knownDialects)
-
-
-    def gotItem(self, item):
-        l = self.listStack
-        if l:
-            l[-1][1].append(item)
-        else:
-            self.callExpressionReceived(item)
-
-    buffer = ''
-
-    def dataReceived(self, chunk):
-        buffer = self.buffer + chunk
-        listStack = self.listStack
-        gotItem = self.gotItem
-        while buffer:
-            assert self.buffer != buffer, "This ain't right: %s %s" % (repr(self.buffer), repr(buffer))
-            self.buffer = buffer
-            pos = 0
-            for ch in buffer:
-                if ch >= HIGH_BIT_SET:
-                    break
-                pos = pos + 1
-            else:
-                if pos > self.prefixLimit:
-                    raise BananaError("Security precaution: more than %d bytes of prefix" % (self.prefixLimit,))
-                return
-            num = buffer[:pos]
-            typebyte = buffer[pos]
-            rest = buffer[pos+1:]
-            if len(num) > self.prefixLimit:
-                raise BananaError("Security precaution: longer than %d bytes worth of prefix" % (self.prefixLimit,))
-            if typebyte == LIST:
-                num = b1282int(num)
-                if num > SIZE_LIMIT:
-                    raise BananaError("Security precaution: List too long.")
-                listStack.append((num, []))
-                buffer = rest
-            elif typebyte == STRING:
-                num = b1282int(num)
-                if num > SIZE_LIMIT:
-                    raise BananaError("Security precaution: String too long.")
-                if len(rest) >= num:
-                    buffer = rest[num:]
-                    gotItem(rest[:num])
-                else:
-                    return
-            elif typebyte == INT:
-                buffer = rest
-                num = b1282int(num)
-                gotItem(num)
-            elif typebyte == LONGINT:
-                buffer = rest
-                num = b1282int(num)
-                gotItem(num)
-            elif typebyte == LONGNEG:
-                buffer = rest
-                num = b1282int(num)
-                gotItem(-num)
-            elif typebyte == NEG:
-                buffer = rest
-                num = -b1282int(num)
-                gotItem(num)
-            elif typebyte == VOCAB:
-                buffer = rest
-                num = b1282int(num)
-                gotItem(self.incomingVocabulary[num])
-            elif typebyte == FLOAT:
-                if len(rest) >= 8:
-                    buffer = rest[8:]
-                    gotItem(struct.unpack("!d", rest[:8])[0])
-                else:
-                    return
-            else:
-                raise NotImplementedError(("Invalid Type Byte %r" % (typebyte,)))
-            while listStack and (len(listStack[-1][1]) == listStack[-1][0]):
-                item = listStack.pop()[1]
-                gotItem(item)
-        self.buffer = ''
-
-
-    def expressionReceived(self, lst):
-        """Called when an expression (list, string, or int) is received.
-        """
-        raise NotImplementedError()
-
-
-    outgoingVocabulary = {
-        # Jelly Data Types
-        'None'           :  1,
-        'class'          :  2,
-        'dereference'    :  3,
-        'reference'      :  4,
-        'dictionary'     :  5,
-        'function'       :  6,
-        'instance'       :  7,
-        'list'           :  8,
-        'module'         :  9,
-        'persistent'     : 10,
-        'tuple'          : 11,
-        'unpersistable'  : 12,
-
-        # PB Data Types
-        'copy'           : 13,
-        'cache'          : 14,
-        'cached'         : 15,
-        'remote'         : 16,
-        'local'          : 17,
-        'lcache'         : 18,
-
-        # PB Protocol Messages
-        'version'        : 19,
-        'login'          : 20,
-        'password'       : 21,
-        'challenge'      : 22,
-        'logged_in'      : 23,
-        'not_logged_in'  : 24,
-        'cachemessage'   : 25,
-        'message'        : 26,
-        'answer'         : 27,
-        'error'          : 28,
-        'decref'         : 29,
-        'decache'        : 30,
-        'uncache'        : 31,
-        }
-
-    incomingVocabulary = {}
-    for k, v in outgoingVocabulary.items():
-        incomingVocabulary[v] = k
-
-    def __init__(self, isClient=1):
-        self.listStack = []
-        self.outgoingSymbols = copy.copy(self.outgoingVocabulary)
-        self.outgoingSymbolCount = 0
-        self.isClient = isClient
-
-    def sendEncoded(self, obj):
-        io = cStringIO.StringIO()
-        self._encode(obj, io.write)
-        value = io.getvalue()
-        self.transport.write(value)
-
-    def _encode(self, obj, write):
-        if isinstance(obj, (list, tuple)):
-            if len(obj) > SIZE_LIMIT:
-                raise BananaError(
-                    "list/tuple is too long to send (%d)" % (len(obj),))
-            int2b128(len(obj), write)
-            write(LIST)
-            for elem in obj:
-                self._encode(elem, write)
-        elif isinstance(obj, (int, long)):
-            if obj < self._smallestLongInt or obj > self._largestLongInt:
-                raise BananaError(
-                    "int/long is too large to send (%d)" % (obj,))
-            if obj < self._smallestInt:
-                int2b128(-obj, write)
-                write(LONGNEG)
-            elif obj < 0:
-                int2b128(-obj, write)
-                write(NEG)
-            elif obj <= self._largestInt:
-                int2b128(obj, write)
-                write(INT)
-            else:
-                int2b128(obj, write)
-                write(LONGINT)
-        elif isinstance(obj, float):
-            write(FLOAT)
-            write(struct.pack("!d", obj))
-        elif isinstance(obj, str):
-            # TODO: an API for extending banana...
-            if self.currentDialect == "pb" and obj in self.outgoingSymbols:
-                symbolID = self.outgoingSymbols[obj]
-                int2b128(symbolID, write)
-                write(VOCAB)
-            else:
-                if len(obj) > SIZE_LIMIT:
-                    raise BananaError(
-                        "string is too long to send (%d)" % (len(obj),))
-                int2b128(len(obj), write)
-                write(STRING)
-                write(obj)
-        else:
-            raise BananaError("could not send object: %r" % (obj,))
-
-
-# For use from the interactive interpreter
-_i = Banana()
-_i.connectionMade()
-_i._selectDialect("none")
-
-
-def encode(lst):
-    """Encode a list s-expression."""
-    io = cStringIO.StringIO()
-    _i.transport = io
-    _i.sendEncoded(lst)
-    return io.getvalue()
-
-
-def decode(st):
-    """
-    Decode a banana-encoded string.
-    """
-    l = []
-    _i.expressionReceived = l.append
-    try:
-        _i.dataReceived(st)
-    finally:
-        _i.buffer = ''
-        del _i.expressionReceived
-    return l[0]
+        self._largestLongInt = 2 ** (limit * 7)
