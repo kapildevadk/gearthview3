9ad590cea91b8e107cb2790a261526f0bd861642
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:56 +0545

    improve

--- ext-libs/twisted/conch/test/test_scripts.py
+++ ext-libs/twisted/conch/test/test_scripts.py
@@ -5,71 +5,50 @@
 Tests for the command-line interfaces to conch.
 """
 
-try:
-    import pyasn1
-except ImportError:
-    pyasn1Skip =  "Cannot run without PyASN1"
-else:
-    pyasn1Skip = None
-
-try:
-    import Crypto
-except ImportError:
-    cryptoSkip = "can't run w/o PyCrypto"
-else:
-    cryptoSkip = None
-
-try:
-    import tty
-except ImportError:
-    ttySkip = "can't run w/o tty"
-else:
-    ttySkip = None
-
-try:
-    import Tkinter
-except ImportError:
-    tkskip = "can't run w/o Tkinter"
-else:
-    try:
-        Tkinter.Tk().destroy()
-    except Tkinter.TclError, e:
-        tkskip = "Can't test Tkinter: " + str(e)
-    else:
-        tkskip = None
-
+import os
+import pyasn1
+import Crypto
+import tty
+import Tkinter
+from collections import defaultdict
 from twisted.trial.unittest import TestCase
 from twisted.scripts.test.test_scripts import ScriptTestsMixin
 from twisted.python.test.test_shellcomp import ZshScriptTestMixin
-
-
 
 class ScriptTests(TestCase, ScriptTestsMixin):
     """
     Tests for the Conch scripts.
     """
-    skip = pyasn1Skip or cryptoSkip
+    skip = defaultdict(str)
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.skip['pyasn1'] = 'Cannot run without PyASN1' if not pyasn1 else ''
+        cls.skip['crypto'] = 'can\'t run w/o PyCrypto' if not Crypto else ''
+        cls.skip['tty'] = 'can\'t run w/o tty' if not tty else ''
+        try:
+            Tkinter.Tk().destroy()
+        except Tkinter.TclError as e:
+            cls.skip['tk'] = f'Can\'t test Tkinter: {e}'
+        else:
+            cls.skip['tk'] = ''
 
+    @unittest.skipIf(skip['tty'] or skip['crypto'], 'Tests require tty and PyCrypto')
     def test_conch(self):
         self.scriptTest("conch/conch")
-    test_conch.skip = ttySkip or skip
 
-
+    @unittest.skipIf(skip['tty'] or skip['crypto'], 'Tests require tty and PyCrypto')
     def test_cftp(self):
         self.scriptTest("conch/cftp")
-    test_cftp.skip = ttySkip or skip
 
-
+    @unittest.skipIf(skip['tty'], 'Tests require tty')
     def test_ckeygen(self):
         self.scriptTest("conch/ckeygen")
 
-
+    @unittest.skipIf(skip['tk'] or skip['tty'], 'Tests require Tkinter and tty')
     def test_tkconch(self):
         self.scriptTest("conch/tkconch")
-    test_tkconch.skip = tkskip or skip
-
-
 
 class ZshIntegrationTestCase(TestCase, ZshScriptTestMixin):
     """
@@ -77,6 +56,4 @@ class ZshIntegrationTestCase(TestCase, ZshScriptTestMixin):
     """
     generateFor = [('conch', 'twisted.conch.scripts.conch.ClientOptions'),
                    ('cftp', 'twisted.conch.scripts.cftp.ClientOptions'),
-                   ('ckeygen', 'twisted.conch.scripts.ckeygen.GeneralOptions'),
-                   ('tkconch', 'twisted.conch.scripts.tkconch.GeneralOptions'),
-                   ]
+                   ('
