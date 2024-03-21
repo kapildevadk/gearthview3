778fe3758657cf268d6308a92f4e43c1d1cec3eb
Kapil <kapildevadk@gmail.com>
2024-03-21 18:01:59 +0545

    improve

--- ext-libs/twisted/plugins/cred_sshkeys.py
+++ ext-libs/twisted/plugins/cred_sshkeys.py
@@ -12,7 +12,10 @@ from zope.interface import implements
 from twisted import plugin
 from twisted.cred.strcred import ICheckerFactory
 from twisted.cred.credentials import ISSHPrivateKey
+from twisted.conch.checkers import SSHPublicKeyDatabase
 
+__author__ = "Twisted Matrix Laboratories"
+__license__ = "MIT"
 
 sshKeyCheckerFactoryHelp = """
 This allows SSH public key authentication, based on public keys listed in
@@ -20,32 +23,28 @@ authorized_keys and authorized_keys2 files in user .ssh/ directories.
 """
 
 
-try:
-    from twisted.conch.checkers import SSHPublicKeyDatabase
+class SSHKeyCheckerFactory(object):
+    """
+    Generates checkers that will authenticate a SSH public key
+    """
+    implements(ICheckerFactory, plugin.IPlugin)
+    authType = 'sshkey'
+    authHelp = sshKeyCheckerFactoryHelp
+    argStringFormat = 'No argstring required.'
+    credentialInterfaces = (ISSHPrivateKey,)
 
-    class SSHKeyCheckerFactory(object):
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        cls.credentialInterfaces = (ISSHPrivateKey,)
+
+    def generateChecker(self, argstring=''):
         """
-        Generates checkers that will authenticate a SSH public key
+        This checker factory ignores the argument string. Everything
+        needed to authenticate users is pulled out of the public keys
+        listed in user .ssh/ directories.
         """
-        implements(ICheckerFactory, plugin.IPlugin)
-        authType = 'sshkey'
-        authHelp = sshKeyCheckerFactoryHelp
-        argStringFormat = 'No argstring required.'
-        credentialInterfaces = SSHPublicKeyDatabase.credentialInterfaces
+        return SSHPublicKeyDatabase()
 
 
-        def generateChecker(self, argstring=''):
-            """
-            This checker factory ignores the argument string. Everything
-            needed to authenticate users is pulled out of the public keys
-            listed in user .ssh/ directories.
-            """
-            return SSHPublicKeyDatabase()
-
-
-
-    theSSHKeyCheckerFactory = SSHKeyCheckerFactory()
-
-except ImportError:
-    # if checkers can't be imported, then there should be no SSH cred plugin
-    pass
+if __name__ == '__main__':
+    print(sshKeyCheckerFactoryHelp)
